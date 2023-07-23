a71498e1d9c17b4e97e30e5a2d202c9e8fbeee88
piriram <pyoram25@gmail.com>
2023-07-24 07:50:51 +0900

    [Refactor]주석 추가

--- S-Badge/AppDelegate.swift
+++ S-Badge/AppDelegate.swift
@@ -10,11 +10,15 @@ import UserNotifications
 
 class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // 1 - UNUserNotificationCenter는 푸시 알림을 포함하여 앱의 모든 알림 관련 활동을 처리합니다.
+        // self x -> background만, self o -> foreground까지
         UNUserNotificationCenter.current().delegate = self
-
+        
         // Request notification authorization
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            // 3 - 완료 핸들러는 인증이 성공했는지 여부를 나타내는 Bool을 수신합니다. 인증 결과를 표시합니다.
             if granted {
+//                완료 핸들러(completion handler)에서 getNotificationSettings()에 대한 호출을 추가했습니다. 이는 사용자가 언제든지 설정 앱으로 이동하여 알림 권한을 변경할 수 있기 때문에 중요합니다. guard 문은 권한이 부여되지 않은 경우 이 호출을 피합니다.
                 print("Notification authorization granted.")
                 DispatchQueue.main.async {
                     UIApplication.shared.registerForRemoteNotifications()

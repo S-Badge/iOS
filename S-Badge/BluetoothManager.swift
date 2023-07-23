50657e98cead89e86aeac14ace948b8accecdab2
piriram <pyoram25@gmail.com>
2023-07-24 07:22:22 +0900

    [Feat]블루투스 연결을 위해 CoreBluetooth 프레임워크 추가

---
+++ S-Badge/BluetoothManager.swift
@@ -0,0 +1,80 @@
+//
+//  BluetoothManager.swift
+//  S-Badge
+//
+//  Created by ram on 2023/07/24.
+//
+import Foundation
+import CoreBluetooth
+import UserNotifications
+
+class BluetoothManager: NSObject, CBCentralManagerDelegate {
+    var centralManager: CBCentralManager!
+    
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            central.scanForPeripherals(withServices: nil, options: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        central.stopScan()
+        central.connect(peripheral, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+    }
+}
+
+extension BluetoothManager: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let services = peripheral.services {
+            for service in services {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                if characteristic.properties.contains(.notify) {
+                    peripheral.setNotifyValue(true, for: characteristic)
+                }
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let data = characteristic.value,
+           let message = String(data: data, encoding: .utf8),
+           message == "1" {
+            sendLocalNotification(titleText: "제목",bodyText: "테스트")
+        }
+    }
+}
+
+func sendLocalNotification(titleText:String,bodyText:String) {
+    let content = UNMutableNotificationContent()
+    content.title = titleText
+    content.body = bodyText
+    content.sound = UNNotificationSound.default
+
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+    UNUserNotificationCenter.current().add(request) { error in
+        if let error = error {
+            print("Error adding notification: \(error.localizedDescription)")
+        } else {
+            print("푸시 알림이 성공적으로 보내졌습니다.")
+        }
+    }
+}

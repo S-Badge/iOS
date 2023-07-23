3c580e3477f84bea30fabd0c661a38a23277b0ed
ram1119 <pyoram25@gmail.com>
2023-07-23 23:53:33 +0900

    Initial Commit

---
+++ S-Badge/S_BadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  S_BadgeApp.swift
+//  S-Badge
+//
+//  Created by ram on 2023/07/23.
+//
+
+import SwiftUI
+
+@main
+struct S_BadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

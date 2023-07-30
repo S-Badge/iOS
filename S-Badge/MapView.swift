8c2c7f339ecfa189b69615387b1f58f7ccb78bc6
piriram <pyoram25@gmail.com>
2023-07-30 22:40:00 +0900

    [Feat]소리알림이 울리지않음

--- S-Badge/MapView.swift
+++ S-Badge/MapView.swift
@@ -5,9 +5,9 @@
 //  Created by ram on 2023/07/29.
 //
 
-import Foundation
 import SwiftUI
 import UserNotifications
+import AVFoundation
 
 struct PushTestView: View {
     var body: some View {
@@ -23,23 +23,50 @@ struct PushTestView: View {
             }
             .padding()
         }
-    }
-
-    // 푸시 알림을 전송하는 함수
-    func sendLocalNotification(titleText: String, bodyText: String) {
-        let content = UNMutableNotificationContent()
-        content.title = titleText
-        content.body = bodyText
-        content.sound = UNNotificationSound.default
-
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-        let request = UNNotificationRequest(identifier: "pushNotification", content: content, trigger: trigger)
-
-        UNUserNotificationCenter.current().add(request) { error in
-            if let error = error {
-                print("Error sending local notification: \(error.localizedDescription)")
-            }
+        .onAppear {
+            UNUserNotificationCenter.current().delegate = NotificationDelegate()
         }
     }
 }
 
+// Function to send local notification
+func sendLocalNotification(titleText: String, bodyText: String) {
+    let center = UNUserNotificationCenter.current()
+
+    let content = UNMutableNotificationContent()
+    content.title = titleText
+    content.body = bodyText
+    content.sound = .default
+
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+    center.add(request) { error in
+        if let error = error {
+            print("Error sending notification: \(error.localizedDescription)")
+        } else {
+            print("Notification sent successfully.")
+        }
+    }
+}
+
+// Notification Delegate to handle notifications when app is in background or foreground
+class NotificationDelegate: NSObject, UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        // When notification is received, wait for 5 seconds and then read the notification content
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.readNotificationContent(response.notification)
+        }
+        completionHandler()
+    }
+
+    private func readNotificationContent(_ notification: UNNotification) {
+        let content = notification.request.content
+        let synthesizer = AVSpeechSynthesizer()
+
+        let utterance = AVSpeechUtterance(string: content.body)
+        utterance.voice = AVSpeechSynthesisVoice(language: "ko-KR") // Change to your preferred language
+
+        synthesizer.speak(utterance)
+    }
+}

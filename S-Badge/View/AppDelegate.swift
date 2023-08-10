2c3b1ab7ade04548bf03c81a7cfaa4534774ba81
piriram <pyoram25@gmail.com>
2023-08-10 16:10:10 +0900

    [Feat]Firebase를 통한 APNs 설정 완료

--- S-Badge/View/AppDelegate.swift
+++ S-Badge/View/AppDelegate.swift
@@ -2,10 +2,21 @@ import UIKit
 import UserNotifications
 import Intents
 import FirebaseCore
-
-class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+import Firebase
+import FirebaseMessaging
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+
+                UNUserNotificationCenter.current().delegate = self
+                let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+                UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { granted, _ in
+                        if granted {
+                            print("알림 등록이 완료되었습니다.")
+                        }
+                    }
+                application.registerForRemoteNotifications()
         // 1 - UNUserNotificationCenter는 푸시 알림을 포함하여 앱의 모든 알림 관련 활동을 처리합니다.
         // self x -> background만, self o -> foreground까지
         UNUserNotificationCenter.current().delegate = self
@@ -48,6 +59,17 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
         print("App opened via tapping the notification.")
         completionHandler()
     }
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+            // Called when a new scene session is being created.
+            // Use this method to select a configuration to create the new scene with.
+            return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        }
+
+        func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+            // Called when the user discards a scene session.
+            // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+            // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        }
     
 }
 
